7e22bc1516f1df8e6fe64d8342c7090d0e3db0a5
Veer Shah <57320962+Veershah26@users.noreply.github.com>
2021-04-18 21:26:55 +0530

    Update qr_code_generator.py

--- Python/qr_code_generator.py
+++ Python/qr_code_generator.py
@@ -1,1 +1,18 @@
+import qrcode
 
+input_URL = input("Enter URL: ")
+
+qr = qrcode.QRCode(
+    version = 1,
+    error_correction = qrcode.constants.ERROR_CORRECT_L,
+    box_size = 5,
+    border = 1,
+)
+
+qr.add_data(input_URL)
+qr.make(fit = False)
+
+img = qr.make_image(fill_color = "black", back_color = "white")
+img.save("qrcode.png")
+
+print(qr.data_list)
